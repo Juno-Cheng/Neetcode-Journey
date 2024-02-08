a8fb69734e69eb39bd1f5ddc31d1073aad552be1
Juno-Cheng <chengjonathan2021@gmail.com>
2024-02-08 11:58:32 -0800

    First Question Completed - NeetCode Journey Begins

---
+++ Arrays-Hashing/Easy-ContainsDuplicates.py
@@ -0,0 +1,37 @@
+#https://leetcode.com/problems/contains-duplicate/description/
+
+#Default Libraries -------------
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+'''
+Question:
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+'''
+#Solution:
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hashMap = {} #Hashmap Dict is created
+        for i in nums: #For each value
+            if i in hashMap: #If exist in Hashmap - Lookup O(1)
+                return True
+            hashMap[i] = 1
+        return False
+    
+#Accidently Reversed the False/True :3 - Whoops
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Neetcode-Journey
 Welcome to my NeetCode Solutions repository! This space is dedicated to documenting my journey through the world of coding interviews, with a focus on mastering data structures and algorithms through the structured approach provided by NeetCode.
+
+Each Folder is a Structure/Algorithm in CS with each question labelled as: Difficulty-QuestionName
