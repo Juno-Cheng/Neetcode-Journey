9ea55e3d002f6bea188c852e84c60baa9f768c54
Juno-Cheng <chengjonathan2021@gmail.com>
2024-03-22 20:07:02 -0700

    Practice

--- Review/2024-03-22-leetcodes-review.py
+++ Review/2024-03-22-leetcodes-review.py
@@ -4,3 +4,43 @@ Date: 2024-03-22
 
 This file contains review notes and solutions for LeetCode problems.
 """
+
+
+#217. Contains Duplicate
+'''Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+'''
+class Solution(object):
+    def containsDuplicate1(self, nums):
+        if len(set(nums)) == len(nums):
+            return True
+        return False
+    
+    def containsDuplicate2(self, nums):
+        dupe = []
+        for value in nums:
+            if value in dupe:
+                return False
+            dupe.append(value)
+        return True
+    
+    def containsDuplicate3(self, nums):
+        hashMap = {}
+        for value in nums:
+            hashMap[value] = hashMap.get(value, 0) + 1
+            if hashMap[value] > 1:
+                return False
+        return True
+    
+
+
