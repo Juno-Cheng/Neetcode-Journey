edf170c6381b9cee65395db2b67a45709bb5f8e1
Juno-Cheng <chengjonathan2021@gmail.com>
2024-03-25 16:57:21 -0700

    Review

--- Review/2024-03-24-leetcodes-review.py
+++ Review/2024-03-24-leetcodes-review.py
@@ -4,3 +4,39 @@ Date: 2024-03-24
 
 This file contains review notes and solutions for LeetCode problems.
 """
+
+#242. Valid Anagram
+'''
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+'''
+
+class Solution(object):
+    def isAnagram1(self, s, t): #44 ms - Beats 16% - Very Sl
+        if len(s) != len(t):
+            return False
+        if set(s) != set(t):
+            return False
+        
+        x,y = sorted(s), sorted(t)
+        return x == y
+
+    def isAnagram2(self, s, t):
+        if len(s) != len(t):
+            return False
+        if set(s) != set(t):
+            return False
+        
+        x,y = sorted(s), sorted(t)
+        return x == y
+   
