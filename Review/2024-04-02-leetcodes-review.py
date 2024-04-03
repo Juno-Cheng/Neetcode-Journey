d3ad89e38b0961a556677042437db683cfdccca4
Jonathan Cheng <chengjonathan2021@gmail.com>
2024-04-03 00:53:40 -0700

    4/3 - Commits

--- Review/2024-04-02-leetcodes-review.py
+++ Review/2024-04-02-leetcodes-review.py
@@ -58,13 +58,5 @@ input2 = filter(lambda x: x["age"] > 20, input)
 y = map(lambda x: x["name"], list(input2))
 print(list(y))
 
-'''
-Problem: Transform to Dictionary
-Given a list of tuples where each tuple contains a name followed by a score, use a lambda function to transform this list into a dictionary where each key is the name, and the value is the score multiplied by 10 if the score is below 5, and by 5 otherwise.
-
-Example Input: [("Alice", 4), ("Bob", 6), ("Charlie", 2)]
-Expected Output: {"Alice": 40, "Bob": 30, "Charlie": 20}
-
-'''
 
 
